7ddd11879146a3f71ac27e56ad48e244e2206588
hrmrakib <hrmrakibs@gmail.com>
2024-10-05 20:44:25 +0600

    include server deployed url

--- src/hooks/useAxiosPublic.tsx
+++ src/hooks/useAxiosPublic.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosPublic = axios.create({
-  baseURL: "http://localhost:4000",
+  // baseURL: "http://localhost:4000",
+  baseURL: "https://airbnb-backend-six.vercel.app",
 });
 
 const useAxiosPublic = () => {
